aacc3728b5fb4514eac87833564df28464220930
Abász <>
2023-03-17 19:06:24 +0100

    initial commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,26 @@
+import { TestBed } from "@angular/core/testing";
+
+import { AppComponent } from "./app.component";
+
+describe("AppComponent", (): void => {
+    beforeEach(async (): Promise<void> => {
+        await TestBed.configureTestingModule({
+            declarations: [AppComponent],
+        }).compileComponents();
+    });
+
+    it("should create the app", (): void => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
+    });
+
+    it("should render title", (): void => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector(".content span")?.textContent).toContain(
+            "esp-rowing-monitor-client app is running!"
+        );
+    });
+});
